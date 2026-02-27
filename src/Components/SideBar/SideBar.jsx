94c6f21a89a34e788497fd255257b1baf71498db
Nacho_Dev <ignacioaugusto155@gmail.com>
2026-02-26 23:16:14 -0300

    Importacion ok

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router"; 
 import { useChat } from "../../Context/ChatContext";
-import './Sidebar.css';
+import './SideBar.css';
 
 const Sidebar = () => {
     const { contacts, setUserName } = useChat(); 
